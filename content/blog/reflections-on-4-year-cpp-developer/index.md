ad4abe4d7022765bae8259ae4af93f52f604fff6
Mariia Kornieva <mariiakornieva@gmail.com>
2021-05-30 07:49:52 +0300

    Update Bio and title

---
+++ content/blog/reflections-on-4-year-cpp-developer/index.md
@@ -0,0 +1,11 @@
+---
+title: Reflections on working 4 years as a C++ developer
+date: "2021-05-29T22:40:32.169Z"
+description: Regrets and learnings
+---
+
+```js
+console.log(hi);
+```
+
+![C++ Programming Language logo](./cpp.jpeg)
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `My Corner on the Internet`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Mariia Kornieva`,
+      summary: ``,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `mariiakornieva`,
     },
   },
   plugins: [
