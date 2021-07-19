0ebea66ed07f17ac4d9e5fc63db2afc9737aeda0
Mariia Kornieva <mariiakornieva@gmail.com>
2021-07-19 09:44:11 +0300

    Add separate blog page and projects page

---
+++ src/pages/projects.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { PageProps } from "gatsby";
+
+const ProjectsPage = (props: PageProps) => {
+  return (
+    <h1>My Projects will be listed here</h1>
+  );
+};
+
+export default ProjectsPage;
